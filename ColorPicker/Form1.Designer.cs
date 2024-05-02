ed80f98445095a05d34cf53e9ecdb321721d8136
George Ongoro <125819670+Joojmusic@users.noreply.github.com>
2024-05-02 13:27:54 +0300

    Add files via upload

---
+++ ColorPicker/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace ColorPicker
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownRed = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelPreview = new System.Windows.Forms.Panel();
+            this.numericUpDownGreen = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownBlue = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBoxConverter = new System.Windows.Forms.ComboBox();
+            this.panelPreview2 = new System.Windows.Forms.Panel();
+            this.listBoxColorList = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.colorListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copySelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.privacyPoliciesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.termsAndLicenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutBestColorPickerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Preview Color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // numericUpDownRed
+            // 
+            this.numericUpDownRed.Location = new System.Drawing.Point(38, 160);
+            this.numericUpDownRed.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownRed.Name = "numericUpDownRed";
+            this.numericUpDownRed.Size = new System.Drawing.Size(50, 28);
+            this.numericUpDownRed.TabIndex = 1;
+            this.numericUpDownRed.ValueChanged += new System.EventHandler(this.numericUpDownRed_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(63)))), ((int)(((byte)(80)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(128)))), ((int)(((byte)(160)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(201, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelPreview
+            // 
+            this.panelPreview.Location = new System.Drawing.Point(16, 54);
+            this.panelPreview.Name = "panelPreview";
+            this.panelPreview.Size = new System.Drawing.Size(258, 100);
+            this.panelPreview.TabIndex = 3;
+            // 
+            // numericUpDownGreen
+            // 
+            this.numericUpDownGreen.Location = new System.Drawing.Point(137, 160);
+            this.numericUpDownGreen.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownGreen.Name = "numericUpDownGreen";
+            this.numericUpDownGreen.Size = new System.Drawing.Size(50, 28);
+            this.numericUpDownGreen.TabIndex = 1;
+            this.numericUpDownGreen.ValueChanged += new System.EventHandler(this.numericUpDownRed_ValueChanged);
+            // 
+            // numericUpDownBlue
+            // 
+            this.numericUpDownBlue.Location = new System.Drawing.Point(223, 160);
+            this.numericUpDownBlue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownBlue.Name = "numericUpDownBlue";
+            this.numericUpDownBlue.Size = new System.Drawing.Size(50, 28);
+            this.numericUpDownBlue.TabIndex = 1;
+            this.numericUpDownBlue.ValueChanged += new System.EventHandler(this.numericUpDownRed_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "R";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "G";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(197, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 21);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "B";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 248);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 28);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // comboBoxConverter
+            // 
+            this.comboBoxConverter.FormattingEnabled = true;
+            this.comboBoxConverter.Items.AddRange(new object[] {
+            "HEX",
+            "aRGB"});
+            this.comboBoxConverter.Location = new System.Drawing.Point(16, 204);
+            this.comboBoxConverter.Name = "comboBoxConverter";
+            this.comboBoxConverter.Size = new System.Drawing.Size(257, 29);
+            this.comboBoxConverter.TabIndex = 5;
+            this.comboBoxConverter.SelectedIndexChanged += new System.EventHandler(this.comboBoxConverter_SelectedIndexChanged);
+            // 
+            // panelPreview2
+            // 
+            this.panelPreview2.Location = new System.Drawing.Point(280, 54);
+            this.panelPreview2.Name = "panelPreview2";
+            this.panelPreview2.Size = new System.Drawing.Size(207, 100);
+            this.panelPreview2.TabIndex = 3;
+            // 
+            // listBoxColorList
+            // 
+            this.listBoxColorList.FormattingEnabled = true;
+            this.listBoxColorList.ItemHeight = 21;
+            this.listBoxColorList.Location = new System.Drawing.Point(282, 162);
+            this.listBoxColorList.Name = "listBoxColorList";
+            this.listBoxColorList.Size = new System.Drawing.Size(205, 172);
+            this.listBoxColorList.TabIndex = 6;
+            this.listBoxColorList.SelectedIndexChanged += new System.EventHandler(this.listBoxColorList_SelectedIndexChanged);
+            this.listBoxColorList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxColorList_MouseDoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(63)))), ((int)(((byte)(80)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(128)))), ((int)(((byte)(160)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button2.Location = new System.Drawing.Point(472, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorListToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(515, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // colorListToolStripMenuItem
+            // 
+            this.colorListToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copySelectedToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.clearAllToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.colorListToolStripMenuItem.Name = "colorListToolStripMenuItem";
+            this.colorListToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.colorListToolStripMenuItem.Text = "Color List";
+            // 
+            // copySelectedToolStripMenuItem
+            // 
+            this.copySelectedToolStripMenuItem.Name = "copySelectedToolStripMenuItem";
+            this.copySelectedToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copySelectedToolStripMenuItem.Text = "Copy Selected";
+            this.copySelectedToolStripMenuItem.Click += new System.EventHandler(this.copySelectedToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearAllToolStripMenuItem.Text = "Clear All";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.privacyPoliciesToolStripMenuItem,
+            this.termsAndLicenceToolStripMenuItem,
+            this.aboutBestColorPickerToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // privacyPoliciesToolStripMenuItem
+            // 
+            this.privacyPoliciesToolStripMenuItem.Name = "privacyPoliciesToolStripMenuItem";
+            this.privacyPoliciesToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.privacyPoliciesToolStripMenuItem.Text = "Privacy Policies";
+            // 
+            // termsAndLicenceToolStripMenuItem
+            // 
+            this.termsAndLicenceToolStripMenuItem.Name = "termsAndLicenceToolStripMenuItem";
+            this.termsAndLicenceToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.termsAndLicenceToolStripMenuItem.Text = "Terms and Licence";
+            // 
+            // aboutBestColorPickerToolStripMenuItem
+            // 
+            this.aboutBestColorPickerToolStripMenuItem.Name = "aboutBestColorPickerToolStripMenuItem";
+            this.aboutBestColorPickerToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.aboutBestColorPickerToolStripMenuItem.Text = "About Best Color Picker";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(515, 399);
+            this.Controls.Add(this.listBoxColorList);
+            this.Controls.Add(this.comboBoxConverter);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panelPreview2);
+            this.Controls.Add(this.panelPreview);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDownBlue);
+            this.Controls.Add(this.numericUpDownGreen);
+            this.Controls.Add(this.numericUpDownRed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximumSize = new System.Drawing.Size(515, 399);
+            this.MinimumSize = new System.Drawing.Size(515, 399);
+            this.Name = "MainWindow";
+            this.Text = "Color Picker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownRed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelPreview;
+        private System.Windows.Forms.NumericUpDown numericUpDownGreen;
+        private System.Windows.Forms.NumericUpDown numericUpDownBlue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBoxConverter;
+        private System.Windows.Forms.Panel panelPreview2;
+        private System.Windows.Forms.ListBox listBoxColorList;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copySelectedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem privacyPoliciesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem termsAndLicenceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutBestColorPickerToolStripMenuItem;
+    }
+}
+
---
+++ ColorPicker/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Fields
+        private readonly List<string> ColorList = new List<string>();
+        private List<string> ColorSchemes = new List<string>();
+        private string ColorListFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "BestColorPicker001", "Color List.txt");
+        string color;
+        Color selectedColor;
+        
+        
+ 
+        // =====================================================//
+        //Methods
+        Color GetColor(Point Location)
+        {
+            using(Bitmap pixelContainer = new Bitmap(1, 1))
+            {
+                using(Graphics graphics = Graphics.FromImage(pixelContainer))
+                {
+                    graphics.CopyFromScreen(Location, Point.Empty, pixelContainer.Size);
+                }
+
+                return pixelContainer.GetPixel(0, 0);
+            }
+
+            
+        }
+
+        private void BackgroundThread()
+        {
+            while (true)
+            {
+                Point cursorPosition = Cursor.Position;
+                selectedColor = GetColor(cursorPosition);
+                panelPreview2.BackColor = selectedColor;
+                string hexValue = ColorTranslator.ToHtml(selectedColor);
+
+                ChangeArgbValues();
+
+                if (comboBoxConverter.SelectedIndex == 0)
+                {
+                    textBox1.Text = hexValue;
+                }
+                else if (comboBoxConverter.SelectedIndex == 1)
+                {
+                    textBox1.Text = selectedColor.R.ToString() + ", " + selectedColor.G.ToString() + ", " + selectedColor.B.ToString();
+                }
+
+                foreach (var listItem in listBoxColorList.Items)
+                {
+                    listBoxColorList.BackColor = selectedColor;
+                }
+            }
+
+            
+        }
+
+        private void AddColorList()
+        {
+            listBoxColorList.Items.Clear();
+
+            foreach(var colorItem in ColorList)
+            {
+                listBoxColorList.Items.Add(colorItem);
+            }
+        }
+
+        private void FillColorSchemes()
+        {
+            ColorSchemes.Add("HEX");
+            ColorSchemes.Add("ARBG");
+        }
+
+        private void ChangeArgbValues()
+        {
+            int red = selectedColor.R;
+            int blue = selectedColor.B;
+            int green = selectedColor.G;
+
+             red = Convert.ToInt32(numericUpDownRed.Value);
+             green = Convert.ToInt32(numericUpDownGreen.Value);
+             blue = Convert.ToInt32(numericUpDownBlue.Value);
+
+            panelPreview.BackColor = Color.FromArgb(selectedColor.A, (byte)red, (byte)green, (byte)blue);
+
+        }
+
+        [DllImport("User32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr intPtr, int wmsg, int wParam, int IParam);
+
+        // ===================================================== //
+        //Events
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(ColorListFilePath))
+            {
+                ColorList.AddRange(File.ReadAllLines(ColorListFilePath));
+            }
+
+            listBoxColorList.Items.Clear();
+
+            foreach (var colorItem in ColorList)
+            {
+                listBoxColorList.Items.Add(colorItem);
+            }
+
+            CheckForIllegalCrossThreadCalls = false;
+            Thread thread = new Thread(BackgroundThread);
+            thread.Start();
+
+            comboBoxConverter.SelectedIndex = 0;
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.ShiftKey)
+            {
+                color = textBox1.Text;
+
+                ColorList.Add(color);
+                AddColorList();
+
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey)
+            {
+                color = textBox1.Text;
+
+                ColorList.Add(color);
+                AddColorList();
+
+            }
+        }
+
+        
+
+        private void comboBoxConverter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBoxConverter.SelectedIndex == 0)
+            {
+                
+            }else if(comboBoxConverter.SelectedIndex == 1)
+            {
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.SelectAll();
+            textBox1.Copy();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void numericUpDownRed_ValueChanged(object sender, EventArgs e)
+        {
+            ChangeArgbValues();
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(ColorListFilePath));
+            File.WriteAllLines(ColorListFilePath, ColorList);
+        }
+
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorList.Clear();
+            listBoxColorList.Items.Clear();
+        }
+
+        private void copySelectedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Copy();
+        }
+
+        private void listBoxColorList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBoxColorList.SelectedItem.ToString();
+        }
+
+        private void listBoxColorList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if(color.ShowDialog() == DialogResult.OK)
+            {
+                string newColor = color.ToString();
+                textBox1.Text = newColor;
+            }
+        }
+
+        // ===================================================== //
+    }
+}
